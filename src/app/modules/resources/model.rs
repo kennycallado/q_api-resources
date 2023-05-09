d0466e919e1d921644d4df0d0ffa40d734c0b75d
kennycallado <kennycallado@hotmail.com>
2023-05-09 12:27:41 +0200

    UPDATE Reestructura modelos

--- src/app/modules/resources/model.rs
+++ src/app/modules/resources/model.rs
@@ -31,10 +31,71 @@ impl From<Resource> for NewResource {
     }
 }
 
+impl From<NewResourceWithNewContent> for NewResource {
+    fn from(resource: NewResourceWithNewContent) -> Self {
+        NewResource {
+            resource_type: resource.resource_type,
+            title: resource.title,
+            description: resource.description,
+        }
+    }
+}
+
+pub struct NewResourceWithNewContent {
+    pub resource_type: String,
+    pub title: String,
+    pub description: String,
+    pub content: Option<NewResourceContent>,
+}
+
+impl From<Resource> for NewResourceWithNewContent {
+    fn from(resource: Resource) -> Self {
+        NewResourceWithNewContent {
+            resource_type: resource.resource_type,
+            title: resource.title,
+            description: resource.description,
+            content: None,
+        }
+    }
+}
+
+impl From<NewResource> for NewResourceWithNewContent {
+    fn from(resource: NewResource) -> Self {
+        NewResourceWithNewContent {
+            resource_type: resource.resource_type,
+            title: resource.title,
+            description: resource.description,
+            content: None,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct ResourceComplete {
+    pub id: i32,
+    pub resource_type: String,
+    pub title: String,
+    pub description: String,
+    pub content: Option<ResourceContentComplete>,
+}
+
+impl From<Resource> for ResourceComplete {
+    fn from(resource: Resource) -> Self {
+        ResourceComplete {
+            id: resource.id,
+            resource_type: resource.resource_type,
+            title: resource.title,
+            description: resource.description,
+            content: None,
+        }
+    }
+}
+
 #[derive(Debug, Clone, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
 pub struct ResourceContent {
-    pub slides: Option<Vec<PubSlide>>,
+    pub slides: Option<Vec<i32>>,
     pub form: Option<i32>,
     pub external: Option<i32>,
 }
@@ -57,77 +118,10 @@ pub struct NewResourceContent {
     pub external: Option<i32>,
 }
 
-// #[derive(Debug, Clone, Deserialize, Serialize)]
-// #[serde(crate = "rocket::serde")]
-// pub struct UpdateResourceContent {
-//     pub slides: Option<Vec<UpdateSlide>>,
-//     pub form: Option<String>,
-//     pub external: Option<String>,
-// }
-
 #[derive(Debug, Clone, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
-pub struct ResourceComplete {
-    pub id: i32,
-    pub resource_type: String,
-    pub title: String,
-    pub description: String,
-    pub content: Option<ResourceContent>,
-    // pub created_at: NaiveDateTime,
-    // pub updated_at: NaiveDateTime,
+pub struct ResourceContentComplete {
+    pub slides: Option<Vec<PubSlide>>,
+    pub form: Option<i32>,
+    pub external: Option<i32>,
 }
-
-impl From<Resource> for ResourceComplete {
-    fn from(resource: Resource) -> Self {
-        ResourceComplete {
-            id: resource.id,
-            resource_type: resource.resource_type,
-            title: resource.title,
-            description: resource.description,
-            content: None,
-            // created_at: resource.created_at,
-            // updated_at: resource.updated_at,
-        }
-    }
-}
-
-// #[derive(Debug, Clone, Deserialize, Serialize)]
-// #[serde(crate = "rocket::serde")]
-// pub struct NewResourceComplete {
-//     pub resource_type: String,
-//     pub title: String,
-//     pub description: String,
-//     pub content: Option<NewResourceContent>,
-// }
-
-// impl From<ResourceComplete> for NewResourceComplete {
-//     fn from(resource: ResourceComplete) -> Self {
-//         NewResourceComplete {
-//             resource_type: resource.resource_type,
-//             title: resource.title,
-//             description: resource.description,
-//             content: match resource.content {
-//                 Some(content) => Some(NewResourceContent {
-//                     slides: match content.slides {
-//                         Some(slides) => {
-//                             Some(slides.into_iter().map(|slide| slide.into()).collect())
-//                         }
-//                         None => None,
-//                     },
-//                     form: content.form,
-//                     external: content.external,
-//                 }),
-//                 None => None,
-//             },
-//         }
-//     }
-// }
-
-// #[derive(Debug, Clone, Deserialize, Serialize)]
-// #[serde(crate = "rocket::serde")]
-// pub struct UpdateResourceComplete {
-//     pub resource_type: String,
-//     pub title: String,
-//     pub description: String,
-//     pub content: Option<UpdateResourceContent>,
-// }
