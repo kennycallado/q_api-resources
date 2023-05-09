f90021f38508ab1508c96862041be7d599563a8c
kennycallado <kennycallado@hotmail.com>
2023-05-09 10:13:33 +0200

    ADD Agrega migración resource_module

---
+++ src/database/migrations/2023-05-09-080939_create_resoruce_module/up.sql
@@ -0,0 +1,15 @@
+-- Modulo es una agrupacion de slides sin questions
+CREATE TABLE IF NOT EXISTS resource_module ();
+
+ALTER TABLE resource_module
+  ADD COLUMN id SERIAL PRIMARY KEY,
+  ADD COLUMN resource_id SERIAL NOT NULL,
+  ADD COLUMN slide_id SERIAL NOT NULL,
+  ADD CONSTRAINT fk_rm_resource_id FOREIGN KEY (resource_id) REFERENCES resources (id) ON DELETE CASCADE;
+
+INSERT INTO resource_module (resource_id, slide_id) VALUES
+  (1, 2),
+  (1, 3),
+  (2, 4),
+  (2, 5)
+  ;
--- src/database/schema.rs
+++ src/database/schema.rs
@@ -1,6 +1,14 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    resource_module (id) {
+        id -> Int4,
+        resource_id -> Int4,
+        slide_id -> Int4,
+    }
+}
+
+diesel::table! {
     resource_slides (id) {
         id -> Int4,
         resource_id -> Int4,
@@ -14,9 +22,16 @@ diesel::table! {
         resource_type -> Varchar,
         title -> Varchar,
         description -> Varchar,
+        created_at -> Timestamptz,
+        updated_at -> Timestamptz,
     }
 }
 
+diesel::joinable!(resource_module -> resources (resource_id));
 diesel::joinable!(resource_slides -> resources (resource_id));
 
-diesel::allow_tables_to_appear_in_same_query!(resource_slides, resources,);
+diesel::allow_tables_to_appear_in_same_query!(
+    resource_module,
+    resource_slides,
+    resources,
+);
